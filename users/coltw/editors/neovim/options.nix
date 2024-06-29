aa28e94bc0745244ef8735702b823acb714da951
Colt Whitley <coltwhitley3@gmail.com>
2024-06-28 22:17:08 -0600

    Added tab options for neovim

--- users/coltw/editors/neovim/options.nix
+++ users/coltw/editors/neovim/options.nix
@@ -2,9 +2,15 @@
   programs.nixvim = {
     # Neovim options
     opts = {
-      #Line Numbers
+      # Line Numbers
       relativenumber = true; # Relative line number
       number = true; # Display absolute number of current line
-    }; 
+
+      # Tab Options 
+      tabstop = 4; # Number of spaces a <Tab> in the text stands for (local to buffer)
+      shiftwidth = 4; # Number of spaces used for each step of (auto)indent (local to buffer)
+      expandtab = true; # Expand <Tab> to spaces in Insert mode (local to buffer)
+      autoindent = true; # Do clever autoindenting
+      }; 
   };
 }
