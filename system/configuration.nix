0ca5c31cefbc116209c1998abefaa20e8c1191d8
Colt Whitley <coltwhitley3@gmail.com>
2024-06-28 19:33:27 -0600

    disabled channesl and changed nixpkg path

--- system/configuration.nix
+++ system/configuration.nix
@@ -42,22 +42,22 @@
 
   # Configure nix and allow flakse
   nix = let
-    # flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
   in {
     settings = {
       # Enable flakes and new 'nix' command
       experimental-features = "nix-command flakes";
       # Opinionated: disable global registry
-      # flake-registry = "";
+      flake-registry = "";
       # Workaround for https://github.com/NixOS/nix/issues/9574
       nix-path = config.nix.nixPath;
     };
     # Opinionated: disable channels
-    # channel.enable = false;
+    channel.enable = false;
 
     # Opinionated: make flake registry and nix path match flake inputs
-    # registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
-    # nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
   };
 
   # Bootloader.
